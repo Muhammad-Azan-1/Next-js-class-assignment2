c0f6701ce5487efc80284ecdcbd14760fcbcc0db
Muhammad Azan <155957067+Muhammad-Azan-1@users.noreply.github.com>
2024-10-16 07:07:40 +0500

    Update main file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
 import Header from './Components/Header/page';
 import Footer from './Components/Footer/page';
 import Hero from './Components/Hero/page';
